942c7a2e6c7625c397059d9da52e641ba99f5f5a
JerrrrrryLee <lijianruilbj@163.com>
2020-06-03 10:26:58 +0800

    complete html templates

---
+++ src/main/java/codedrinker/blog/controller/TestController.java
@@ -0,0 +1,12 @@
+package codedrinker.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/aaa")
+    public String show(){
+        return "blog";
+    }
+}
---
+++ src/test/java/codedrinker/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package codedrinker.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
